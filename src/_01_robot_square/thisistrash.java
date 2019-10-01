e2e3cb83ed614a81f59e8ef80938ecf2d9e447f8
League Student <league@iMac-37.attlocal.net>
2019-09-30 19:53:23 -0700

    Create thisistrash.java

---
+++ src/_01_robot_square/thisistrash.java
@@ -0,0 +1,52 @@
+package _01_robot_square;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class thisistrash {
+	public static void main(String[] args) {
+		Robot dod=new Robot("mini");
+		dod.setSpeed(200);
+		dod.penDown();
+		dod.setRandomPenColor();
+		dod.move(30);
+		dod.turn(90);
+		dod.turn(90);
+		dod.move(60);
+		dod.turn(90);
+		dod.turn(90);
+		dod.move(30);
+		dod.turn(90);
+		dod.turn(90);
+		dod.turn(90);
+		dod.move(80);
+		dod.turn(90);
+		dod.move(30);
+		dod.penUp();
+	
+dod.turn(90);
+dod.move(200);
+dod.turn(90);
+dod.turn(90);
+dod.turn(90);
+dod.turn(90);
+dod.turn(90);
+dod.turn(90);
+dod.turn(90);
+dod.penDown();
+dod.setRandomPenColor();
+dod.move(30);
+dod.turn(90);
+dod.turn(90);
+dod.move(60);
+dod.turn(90);
+dod.turn(90);
+dod.move(30);
+dod.turn(90);
+dod.turn(90);
+dod.turn(90);
+dod.move(80);
+dod.turn(90);
+dod.move(30);
+	}
+
+}
